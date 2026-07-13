1ec62f79edcd30cbea1621b41c7c0f312707f8d3
AryanSingh2677-boop <aryansingh200626@gmail.com>
2026-07-14 00:37:38 +0530

    add

--- iterations/project20.js
+++ iterations/project20.js
@@ -8,12 +8,12 @@
 const arr = [1, 2, 3, 4, 5];
 
 for (const num of arr) {
-    console.log(num);
+    // console.log(num);
 }
 
 const greetings = "hello world";
 for (const greet of greetings) {
-    console.log(`each char of is: ${greet}`);
+    // console.log(`each char of is: ${greet}`);
 }
 
 // maps
@@ -23,5 +23,37 @@ myMap.set("IN", "India")
 myMap.set("US", "United States")
 myMap.set("UK", "United Kingdom")
 
-console.log(myMap);
+// console.log(myMap);
 
+for (const [key, value] of myMap) {//this form is used to destructure the key and value from the map
+    console.log(key, ":-", value);
+}
+
+const myObj = {
+    'game': 'cricket',
+    'country': 'India',
+    'player': 'Virat Kohli'
+}
+
+// for (const [key, value] of myObj) { //this will not work because objects are not iterable
+//     console.log(key, ":-", value);
+// }
+
+const myObject = {
+    js: 'JavaScript',
+    py: 'Python',
+    rb: 'Ruby'
+}
+
+for (const key in myObject) { //this will work because we are using for in loop which is used to iterate over the properties of an object
+    console.log(`${key} shortcut for ${myObject[key]}`);
+}
+
+const programmingLanguages = ['JavaScript', 'Python', 'Ruby', 'Java', 'C++'];
+for (const lang in programmingLanguages) {//when we use for in loop on an array it will give us the index of the array elements
+    console.log(programmingLanguages[lang]);
+}
+
+for (const key in Map) { //this will not work because Map is not an object
+    console.log(key);
+}
